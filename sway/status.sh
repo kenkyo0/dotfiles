1a2d1444c6ecd0b02a3bdd9008447631d347acda
kenko0 <nick.schmidt1711@gmail.com>
2025-12-29 02:32:15 +0100

    swaybar weather status cache fix

--- sway/status.sh
+++ sway/status.sh
@@ -1,23 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 # scripts are located in $HOME/.config/.scripts
 # (they are exported only in zsh)
+helper=$HOME/.config/sway
 pfad=$HOME/.config/.scripts
 time=$(date '+%d-%m-%Y')
 local_ip=$(ip -4 addr show dynamic | grep inet | awk '{print $2}')
-#weather_cache()
-#{   
-#    if [[ ! -f /tmp/weather_old.txt ]]; then
-#        $(echo $(weather) > /tmp/weather_old.txt &)
-#    fi
-#    weather_new=$(weather)
-#    if [[ $(echo ${weather_new} | wc -c) -lt 15 && $(echo ${weather_buf} | wc -c) != 0 ]]; then
-#        $(echo ${weather_new} > /tmp/weather_old.txt)
-#        echo ${weather_new}
-#    else
-#        cat /tmp/weather_old.txt
-#    fi
-#}
+
+weather_cache()
+{   
+    local weather_new
+    weather_new=$(weather)
+
+    if [[ -n "$weather_new" ]]; then
+        echo "${weather_new}" > ${helper}/weather_old.txt
+        echo "$weather_new"
+    else
+        # checking cache file // -s checks if the file is available and bigger than 0
+        if [[ -s "$(cat ${helper}/weather_old.txt)" ]]; then
+            echo '...'   
+        else
+        # use cache
+            cat ${helper}/weather_old.txt
+        fi
+    fi
+}
 
 
-echo "$local_ip   |   $(${pfad}/weather)   |   $(${pfad}/battery)   |   📅 $time   |   $(${pfad}/sb-clock)  "
+echo "$local_ip     |     $(weather_cache)     |     $(${pfad}/battery)     |     📅 $time     |     $(${pfad}/sb-clock)  "
