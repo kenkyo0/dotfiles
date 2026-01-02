4c237063a520936c9895708fd08f605464796473
kenko0 <nick.schmidt1711@gmail.com>
2026-01-02 21:28:15 +0100

    Fixed Treesitter in neovim and added functionality scripts in Mangowc

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,46 +2,29 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<Leader>ms", -- set to false to disable one of the mappings
-                        node_incremental= "<Leader>mi",
-                        scope_incremental = "<Leader>mc",
-                        node_decremental = "<Leader>md",
-                    },
+        lazy = false,
+        opts = {
+            ensure_installed = {
+                "bash", "c", "rust", "cpp", "html", "javascript", "java", "lua",
+                "luadoc", "luap", "markdown", "markdown_inline", "python",
+                "toml", "typescript", "vim", "vimdoc", "xml", "yaml", "query",
+                "hyprlang" -- WICHTIG: Das ist der Parser für Mango/Hyprland Syntax
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Leader>ms",
+                    node_incremental = "<Leader>mi",
+                    scope_incremental = "<Leader>mc",
+                    node_decremental = "<Leader>md",
                 },
-                    auto_install = true,
-
-                    highlight = {
-                        enable = true,
-                    },
-                    ensure_installed = {
-                        "bash",
-                        "c",
-                        "rust",
-                        "cpp",
-                        "html",
-                        "javascript",
-                        "java",
-                        "lua",
-                        --"latex",
-                        "luadoc",
-                        "luap",
-                        "markdown",
-                        "markdown_inline",
-                        "python",
-                        "toml",
-                        "typescript",
-                        "vim",
-                        "vimdoc",
-                        "xml",
-                        "yaml",
-                        "query"
-                    }    })
-        end,
-
-    }
+            },
+            vim.filetype.add({                                     
+                pattern = { [".*/mango/config.conf"] = "hyprlang" }, 
+            })                                                         
+        },
+    },
 }
+
